e37ba185376d5ef4f87f88484cff90db5b9ba9f4
Vedant Vatare <vedantvatare73@gmail.com>
2025-06-17 12:09:37 +0530

    add user profile update

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,17 +4,27 @@ import {
   signup,
   forgotPassword,
   resetPassword,
+  updateProfile,
 } from '../controllers/userController.js';
 import {
   validateSignIn,
   validateSignUp,
   validateForgotPassword,
   validateResetPassword,
+  validateProfileUpdate,
+  authenticateUser,
 } from '../middleware/userValidation.js';
+import { upload } from '../middleware/multer.js';
 const router = Router();
 
 router.post('/signup', validateSignUp, signup);
 router.post('/signin', validateSignIn, signin);
 router.post('/forgot-password', validateForgotPassword, forgotPassword);
 router.post('/reset-password', validateResetPassword, resetPassword);
+router.put(
+  '/update-profile',
+  [authenticateUser, upload.single('avatar'), validateProfileUpdate],
+  updateProfile,
+);
+
 export default router;
