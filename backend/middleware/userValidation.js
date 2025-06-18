3779d90e6fe63f2d68ecaa429ff8aefabf2aeeff
Vedant Vatare <vedantvatare73@gmail.com>
2025-06-18 19:09:24 +0530

    modify user authentication middlware

--- backend/middleware/userValidation.js
+++ backend/middleware/userValidation.js
@@ -55,8 +55,15 @@ async function authenticateAdmin(req, res, next) {
 async function authenticateUser(req, res, next) {
   try {
     const token = req.headers.authorization?.split(' ')[1];
-    const { userId, userType } = verifyJWT(token);
-    if (!userId) return res.status(401).json({ error: 'Invalid token' });
+    const JWTResult = verifyJWT(token);
+    if (JWTResult.error) {
+      return res.status(401).json({ error: 'Invalid token' });
+    }
+    const { userId, userType } = JWTResult;
+    if (!userId) {
+      return res.status(401).json({ error: 'Invalid token' });
+    }
+
     [req.userId, req.userType] = [userId, userType];
     next();
   } catch (e) {
